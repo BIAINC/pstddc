47b7110a47f3a2addb3b5af72dc35c6f1a544388
Paul Morton <pmorton@biaprotect.com>
2014-08-07 13:39:43 -0700

    Add support for managing legal holds

---
+++ samples/manage_legal_hold_flag.ps1
@@ -0,0 +1,16 @@
+Connect-TDDC -AuthToken '<Your Token>'
+Get-Matters -Company <CompanyID> | Get-LegalHolds | Convert-LegalHoldToCustodians | Group-LegalHoldsByCustodian | % {
+    $email = $_.email
+    $user = Get-ADUser -Filter {EmailAddress -like $email}
+    if ($user.count -eq 0) {
+        Write-Warning "User not found in active directory $($custodian.email)"
+    } else {
+        if( $_.is_on_hold ){
+            Write-Host "Setting $($email) to ON legal hold status"
+            Set-aduser -Replace @{description="OnLegalHold"} -Identity $user
+        } else {
+            Write-Host "Setting $($email) to OFF legal hold status"
+            Set-AdUser -Replace @{description="OffKegalHold"} -Identity $user
+        }
+    }
+}
